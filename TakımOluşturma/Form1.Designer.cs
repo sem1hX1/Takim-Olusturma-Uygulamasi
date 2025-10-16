1d11d4af3b35144f9471564efa6943fb81e3d1f3
semih şeker <semihskr42@gmail.com>
2025-10-16 21:54:10 +0300

    Proje tamamlandı: Tüm özellikler eklendi ve son halidir

--- TakımOluşturma/Form1.Designer.cs
+++ TakımOluşturma/Form1.Designer.cs
@@ -29,25 +29,183 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            comboBox1 = new ComboBox();
+            label2 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            listBox1 = new ListBox();
+            listBox2 = new ListBox();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(283, 115);
+            label1.BackColor = Color.Gray;
+            label1.BorderStyle = BorderStyle.FixedSingle;
+            label1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label1.Location = new Point(109, 14);
             label1.Name = "label1";
-            label1.Size = new Size(81, 20);
+            label1.Size = new Size(104, 25);
             label1.TabIndex = 0;
-            label1.Text = "oyuncu adı";
+            label1.Text = "oyuncu adı:";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(219, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(255, 192, 192);
+            button1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            button1.Location = new Point(166, 45);
+            button1.Name = "button1";
+            button1.Size = new Size(104, 29);
+            button1.TabIndex = 2;
+            button1.Text = "Ekle";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(138, 140);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(151, 31);
+            comboBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.FromArgb(255, 192, 192);
+            label2.BorderStyle = BorderStyle.FixedSingle;
+            label2.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label2.Location = new Point(151, 103);
+            label2.Name = "label2";
+            label2.Size = new Size(126, 25);
+            label2.TabIndex = 4;
+            label2.Text = "Oyuncu Listesi";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(255, 192, 192);
+            button2.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            button2.Location = new Point(66, 177);
+            button2.Name = "button2";
+            button2.Size = new Size(104, 29);
+            button2.TabIndex = 5;
+            button2.Text = "A Takımı";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(255, 192, 192);
+            button3.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            button3.Location = new Point(240, 177);
+            button3.Name = "button3";
+            button3.Size = new Size(104, 29);
+            button3.TabIndex = 6;
+            button3.Text = "B Takımı";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(66, 212);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(104, 104);
+            listBox1.TabIndex = 7;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.Location = new Point(240, 212);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(104, 104);
+            listBox2.TabIndex = 8;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Red;
+            button4.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            button4.Location = new Point(66, 322);
+            button4.Name = "button4";
+            button4.Size = new Size(104, 58);
+            button4.TabIndex = 9;
+            button4.Text = "Takımdan Çıkar";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.Red;
+            button5.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            button5.Location = new Point(240, 322);
+            button5.Name = "button5";
+            button5.Size = new Size(104, 58);
+            button5.TabIndex = 10;
+            button5.Text = "Takımdan Çıkar";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.Blue;
+            button6.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            button6.Location = new Point(176, 224);
+            button6.Name = "button6";
+            button6.Size = new Size(58, 37);
+            button6.TabIndex = 11;
+            button6.Text = ">>";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.Blue;
+            button7.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            button7.Location = new Point(176, 267);
+            button7.Name = "button7";
+            button7.Size = new Size(58, 37);
+            button7.TabIndex = 12;
+            button7.Text = "<<";
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += button7_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(464, 450);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(listBox2);
+            Controls.Add(listBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(label2);
+            Controls.Add(comboBox1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
             Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -55,5 +213,17 @@
         #endregion
 
         private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private ComboBox comboBox1;
+        private Label label2;
+        private Button button2;
+        private Button button3;
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
     }
 }
--- TakımOluşturma/Form1.cs
+++ TakımOluşturma/Form1.cs
@@ -6,5 +6,55 @@ namespace TakımOluşturma
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add(textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(comboBox1.Text);
+            comboBox1.Items.Remove(comboBox1.SelectedItem);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Add(comboBox1.Text);
+            comboBox1.Items.Remove(comboBox1.SelectedItem);
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Add(listBox1.SelectedItem);
+            listBox1.Items.Remove(listBox1.SelectedItem);
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(listBox2.SelectedItem);
+            listBox2.Items.Remove(listBox2.SelectedItem);
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add(listBox1.SelectedItem);
+            listBox1.Items.Remove(listBox1.SelectedItem);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add(listBox2.SelectedItem);
+            listBox2.Items.Remove(listBox2.SelectedItem);
+        }
     }
 }
